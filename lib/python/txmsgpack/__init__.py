b0cb182c38a24316d9d39b48abe24ed1475bf45e
Donal McMullan <donal.mcmullan@gmail.com>
2013-04-27 19:55:58 +1200

    Add Trial tests. Remove the remote_echo method from protocol, creating mock Echo protocol instead.

---
+++ lib/python/txmsgpack/__init__.py
@@ -0,0 +1,7 @@
+from protocol import Msgpack
+from protocol import MsgpackError
+from protocol import MsgpackServerFactory
+from protocol import MsgpackClientFactory
+from protocol import MSGTYPE_REQUEST
+from protocol import MSGTYPE_RESPONSE
+from protocol import MSGTYPE_NOTIFICATION
